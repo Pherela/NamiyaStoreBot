f49ce64468cf37655ec073fb7e9259a4262f04c6
* <m.farrellm.farrell.13@gmail.com>
2024-02-13 01:53:36 +0700

    refactoring but introduces some unwanted effect

--- namiya.py
+++ namiya.py
@@ -2,57 +2,44 @@ import os
 import random
 import sqlite3
 from dotenv import load_dotenv
-from telebot import TeleBot, types, util
-
+from telebot import TeleBot, types
 
 class MiracleofNamiyaStoreBot:
     def __init__(self, token, cmds):
         self.bot = TeleBot(token)
         self.roles = ['helper', 'seeker']
-        self.setup_database()
-        self.forwarded_messages = {}
+        self.db = self.setup_database()
         self.cmds = cmds
-        self.bot.message_handler(commands=['start'])(self.assign_role)
-        self.bot.message_handler(commands=['help'])(self.help_page)
-        
+        self.set_handlers()
 
     def setup_database(self):
-        self.conn = sqlite3.connect('user_roles.db', check_same_thread=False)
-        self.cursor = self.conn.cursor()
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS user_roles (
-                chat_id INTEGER PRIMARY_KEY,
-                role TEXT
-            )
-        ''')
-        
-    def set_commands(self):
-        for lc in self.cmds[0].keys() - {'cmd'}:
-            self.bot.set_my_commands([types.BotCommand(cmd['cmd'], cmd[lc]) for cmd in self.cmds], language_code=lc)
-            
-    def help_page(self, message):
-        lang_code = message.from_user.language_code
-        self.cursor.execute('SELECT role FROM user_roles WHERE chat_id = ?', (message.chat.id,))
-        role = self.cursor.fetchone()[0]
-        for cmd in self.cmds:
-            if cmd['cmd'] == 'help':
-                self.bot.send_message(message.chat.id, cmd[f'{lang_code}_{role}'])
+        conn = sqlite3.connect('user_roles.db', check_same_thread=False)
+        cursor = conn.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS user_roles (chat_id INTEGER PRIMARY_KEY, role TEXT)')
+        return conn, cursor
 
+    def set_handlers(self):
+        self.bot.message_handler(commands=['start'])(self.assign_role)
+        self.bot.message_handler(commands=['help'])(self.help_page)
 
-                
+    def get_role(self, chat_id):
+        self.db[1].execute('SELECT role FROM user_roles WHERE chat_id = ?', (chat_id,))
+        return self.db[1].fetchone()[0]
+
+    def send_message(self, chat_id, cmd, role, lang_code):
+        self.bot.send_message(chat_id, cmd[f'{lang_code}_{role}'])
+
     def assign_role(self, message):
         if message.text == '/start':
             random.shuffle(self.roles)
             chosen_role = self.roles[0]
-            self.cursor.execute('REPLACE INTO user_roles VALUES (?, ?)', (message.chat.id, chosen_role))
-            self.conn.commit()
-            for cmd in self.cmds:
-                if cmd['cmd'] == 'start':
-                    if message.from_user.language_code == 'id':
-                        self.bot.send_message(message.chat.id, cmd[f'id_{chosen_role}'])
-                    else:
-                        self.bot.send_message(message.chat.id, cmd[f'en_{chosen_role}'])
-                        
+            self.db[1].execute('REPLACE INTO user_roles VALUES (?, ?)', (message.chat.id, chosen_role))
+            self.db[0].commit()
+            self.send_message(message.chat.id, self.cmds[0], chosen_role, message.from_user.language_code)
+
+    def help_page(self, message):
+        self.send_message(message.chat.id, self.cmds[1], self.get_role(message.chat.id), message.from_user.language_code)
+
     def start_polling(self):
         self.bot.infinity_polling(long_polling_timeout=20)
 
@@ -64,3 +51,4 @@ if __name__ == "__main__":
     ]
     bot = MiracleofNamiyaStoreBot(os.getenv('TELEGRAM_TOKEN'), cmds)
     bot.start_polling()
+
