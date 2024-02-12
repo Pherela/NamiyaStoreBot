9edd57080f85554bdbea6e13ac17267203eac402
Pherela <71290283+Pherela@users.noreply.github.com>
2024-02-12 20:44:19 +0700

    Update namiya.py

--- namiya.py
+++ namiya.py
@@ -6,13 +6,13 @@ from telebot import TeleBot, types, util
 
 
 class MiracleofNamiyaStoreBot:
-    def __init__(self, token):
+    def __init__(self, token, cmds):
         self.bot = TeleBot(token)
         self.user_roles = {}
         self.roles = ['helper', 'seeker']
         self.setup_database()
         self.forwarded_messages = {}
-
+        self.cmds = cmds
         self.bot.message_handler(commands=['start'])(self.assign_role)
 
     def setup_database(self):
@@ -25,26 +25,30 @@ class MiracleofNamiyaStoreBot:
             )
         ''')
         
-    def set_commands(self, cmds):
-        for lc in cmds[0].keys() - {'cmd'}:
-            self.bot.set_my_commands([types.BotCommand(c['cmd'], c[lc]) for c in cmds], language_code=lc)
+    def set_commands(self):
+        for lc in self.cmds[0].keys() - {'cmd'}:
+            self.bot.set_my_commands([types.BotCommand(c['cmd'], c[lc]) for c in self.cmds], language_code=lc)
             
     def assign_role(self, message):
         if message.text == '/start':
             chosen_role = random.choice(self.roles)
             self.cursor.execute('REPLACE INTO user_roles VALUES (?, ?)', (message.chat.id, chosen_role))
             self.conn.commit()
-            self.bot.send_message(message.chat.id, f"You have been randomly assigned the role of {chosen_role}.")
-            
+            for cmd in self.cmds:
+                if cmd['cmd'] == 'start':
+                    if message.from_user.language_code == 'id':
+                        self.bot.send_message(message.chat.id, cmd[f'id_{chosen_role}'])
+                    else:
+                        self.bot.send_message(message.chat.id, cmd[f'en_{chosen_role}'])
+                        
     def start_polling(self):
         self.bot.infinity_polling(long_polling_timeout=20)
 
 if __name__ == "__main__":
     load_dotenv('./.env')
-    bot = MiracleofNamiyaStoreBot(os.getenv('TELEGRAM_TOKEN'))
     cmds = [
-    {"cmd": "start", "en": "Begin", "id": "Mulai"},
+    {"cmd": "start", "en": "Begin", "id": "Mulai", "en_helper": "Welcome to Namiya's Store! Let's provide guidance to those in need.", "id_helper": "Selamat datang di Toko Namiya! Mari kita berikan petunjuk kepada mereka yang membutuhkan.", "en_seeker": "Welcome to Namiya's Store! Let's seek answers to your questions.", "id_seeker": "Selamat datang di Toko Namiya! Mari kita cari petunjuk untuk pertanyaan yang Anda miliki."},
     {"cmd": "help", "en": "Guide on how to use the bot", "id": "Panduan cara menggunakan bot"}
     ]
-    bot.set_commands(cmds)
+    bot = MiracleofNamiyaStoreBot(os.getenv('TELEGRAM_TOKEN'), cmds)
     bot.start_polling()
